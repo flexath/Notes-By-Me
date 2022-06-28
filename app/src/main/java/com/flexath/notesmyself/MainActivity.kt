7f18e874b42d06593f494a2db73e9c2c0de8fe41
flexath <flexath11@gmail.com>
2022-06-28 23:47:31 +0630

    Second Version

--- app/src/main/java/com/flexath/notesmyself/MainActivity.kt
+++ app/src/main/java/com/flexath/notesmyself/MainActivity.kt
@@ -1,33 +1,54 @@
 package com.flexath.notesmyself
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
+import android.view.View
+import android.widget.LinearLayout
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.DefaultItemAnimator
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    private var tempNote = Note()
+    //private var tempNote = Note()
+
+    private val noteList = ArrayList<Note>()
+    private var recyclerView: RecyclerView? = null
+    private var adapter: NoteAdapter? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val buttonShowNote = findViewById<Button>(R.id.btnShowNote)
-
-        buttonShowNote.setOnClickListener {
-            val showDialog = ShowNoteDialog()
-            showDialog.showSelectedNote(tempNote)
-            showDialog.show(supportFragmentManager,"")
-        }
-
         fab.setOnClickListener {
             val newDialog = NewNoteDialog()
             newDialog.show(supportFragmentManager,"123")
+
+            recyclerView = findViewById<View>(R.id.recyclerView) as RecyclerView
+
+            val layout_manager = LinearLayoutManager(applicationContext)
+            recyclerView!!.layoutManager = layout_manager
+            recyclerView!!.itemAnimator = DefaultItemAnimator()
+            recyclerView!!.addItemDecoration(DividerItemDecoration(this, LinearLayout.VERTICAL))
+
+            adapter = NoteAdapter(this,noteList)
+            recyclerView!!.adapter = adapter
         }
     }
 
     fun createNewNote(n : Note) {
-        tempNote = n
+        //tempNote = n
+        noteList.add(n)
+
+        // lets our adapter know that a new note has been added.
+        adapter!!.notifyDataSetChanged()
+    }
+
+    fun showNote(adapterPosition: Int) {
+        val dialog = ShowNoteDialog()
+        dialog.showSelectedNote(noteList[adapterPosition])
+        dialog.show(supportFragmentManager,"")
     }
 }
---
+++ app/src/main/java/com/flexath/notesmyself/NoteAdapter.kt
@@ -0,0 +1,51 @@
+package com.flexath.notesmyself
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class NoteAdapter(private val mainActivity: MainActivity,private val noteList: List<Note>)
+                    : RecyclerView.Adapter<NoteAdapter.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NoteAdapter.ViewHolder {
+        val itemView = LayoutInflater.from(parent.context).inflate(R.layout.list_item,parent,false)
+        return ViewHolder(itemView)
+    }
+
+    override fun onBindViewHolder(holder: NoteAdapter.ViewHolder, position: Int) {
+        val note = noteList[position]
+        holder.itemTitle.text = note.title
+        holder.itemDescription.text = note.description!!.substring(15)
+        when {
+            note.idea -> holder.itemStatus.text = mainActivity.resources.getString(R.string.new_note_idea)
+            note.important -> holder.itemStatus.text = mainActivity.resources.getString(R.string.new_note_important)
+            note.todo -> holder.itemStatus.text = mainActivity.resources.getString(R.string.new_note_todo)
+            else -> holder.itemStatus.text = mainActivity.resources.getString(R.string.blank_text)
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return noteList.size
+    }
+
+    inner class ViewHolder(view : View) : RecyclerView.ViewHolder(view),View.OnClickListener{
+        val itemStatus:TextView
+        val itemTitle:TextView
+        val itemDescription:TextView
+
+        init {
+            itemStatus = view.findViewById(R.id.textViewStatus)
+            itemTitle = view.findViewById(R.id.textViewTitle)
+            itemDescription = view.findViewById(R.id.textViewDescription)
+            view.isClickable = true
+            view.setOnClickListener(this)
+        }
+
+        override fun onClick(v: View?) {
+            mainActivity.showNote(adapterPosition)
+        }
+    }
+
+}
